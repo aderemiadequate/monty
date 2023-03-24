9ab5d8ad99d86c583bc5e1ec411693e61e09e9f4
aderemiadequate <temitopeworkspace@gmail.com>
2023-03-24 13:47:51 -0700

    task

--- get_div.c
+++ get_div.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 
 /**
- * get_mod - function that finds the modulus of top two elements
+ * get_div - function that divides the top two elements of the stack
  * @stack: pointer to the top of the stack
  * @line_number: where the line number appears
- * Description: 9. mod
+ * Description: 7. div
  * Return: see below
  * 1. upon success, nothing
  * 2. upon fail, EXIT_FAILURE
  */
-void get_mod(stack_t **stack, unsigned int line_number)
+void get_div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *first, *second;
 
 	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		fclose(file);
-		get_free(*stack);
+		free(*stack);
 		exit(EXIT_FAILURE);
 	}
 	else if ((*stack)->n == 0)
@@ -31,10 +31,9 @@ void get_mod(stack_t **stack, unsigned int line_number)
 	{
 		first = *stack;
 		second = first->next;
-		second->n %= first->n;
+		second->n /= first->n;
 		*stack = second;
 		(*stack)->prev = NULL;
 		free(first);
 	}
 }
-
--- swap.c
+++ swap.c
@@ -1,31 +1,37 @@
 #include "monty.h"
-/**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_swap(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
+/**
+ * get_swap - function that swaps the top two elements on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
+ * Description: 3. swap
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first, *second;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fclose(file);
+		get_free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	aux = h->n;
-	h->n = h->next->n;
-	h->next->n = aux;
+	else if (*stack)
+	{
+		first = *stack;
+		second = first->next;
+		*stack = second;
+		first->next = second->next;
+		if (second->next != NULL)
+		{
+			second->next->prev = first;
+		}
+		second->prev = NULL;
+		second->next = first;
+		first->prev = second;
+	}
 }
